6de9686a8e67a91b800d8cb330ab444c99a8aa44
Konstantyn <6287026@stud.nau.edu.ua>
2025-01-24 17:40:34 +0200

    test

--- components/F-Review.vue
+++ components/F-Review.vue
@@ -87,6 +87,8 @@ import FInput from '~/components/Inputs/F-Input.vue'
 import FModal from '~/components/F-Modal.vue'
 import * as yup from 'yup'
 
+// test
+
 const props = defineProps<{
   title: string
   rating: number
